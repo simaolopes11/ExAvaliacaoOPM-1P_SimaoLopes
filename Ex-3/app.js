41c0778c266c32e7dc9770b769e5c8760a906454
simaolopes11 <simaolopes111007@gmail.com>
2025-12-09 10:27:09 +0000

    uptdate
    exercícios 1 2 e 3 completos

---
+++ Ex-3/app.js
@@ -0,0 +1,15 @@
+let popup = "Olá Mundo! Vamos imprimir na console uma tabuada tradicional!"
+alert(popup)
+
+let tabuada = prompt("Insere um número")
+
+console.log(tabuada * 1)
+console.log(tabuada * 2)
+console.log(tabuada * 3)
+console.log(tabuada * 4)
+console.log(tabuada * 5)
+console.log(tabuada * 6)
+console.log(tabuada * 7)
+console.log(tabuada * 8)
+console.log(tabuada * 9)
+console.log(tabuada * 10)
