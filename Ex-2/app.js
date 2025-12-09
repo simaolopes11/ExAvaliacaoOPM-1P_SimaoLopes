52ffd76e633f8500faaa6e3bf4ac1f6b06af09a5
simaolopes11 <simaolopes111007@gmail.com>
2025-12-09 09:46:08 +0000

    update
    exercício 1 e 2 completo

---
+++ Ex-2/app.js
@@ -0,0 +1,13 @@
+let popup = "Olá Mundo!"
+alert(popup)
+
+let numero = prompt("Insere um número")
+
+console.log(numero % 2)
+
+if (numero % 2 == 1) {
+    alert("O teu número é impar!")
+} else if (numero % 2 == 0) {
+    alert("O teu número é par!")
+}
+
